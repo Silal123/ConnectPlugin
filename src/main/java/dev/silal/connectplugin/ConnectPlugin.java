f761aaecb3392402aa671b528b0180a82ddd1415
Silal <111568366+Silal123@users.noreply.github.com>
2025-06-08 16:26:40 +0200

    Added Metrics and update checker

--- src/main/java/dev/silal/connectplugin/ConnectPlugin.java
+++ src/main/java/dev/silal/connectplugin/ConnectPlugin.java
@@ -6,9 +6,15 @@ import dev.silal.connectplugin.core.commands.UnlinkCommand;
 import dev.silal.connectplugin.core.commands.tabcompleter.DataCommandTabCompleter;
 import dev.silal.connectplugin.core.connection.DataStorage;
 import dev.silal.connectplugin.core.utils.Configuration;
+import dev.silal.connectplugin.core.utils.JsonManager;
+import dev.silal.connectplugin.core.utils.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public final class ConnectPlugin extends JavaPlugin {
 
@@ -33,6 +39,8 @@ public final class ConnectPlugin extends JavaPlugin {
         return config;
     }
 
+    private Metrics metrics;
+
     @Override
     public void onEnable() {
         instance = this;
@@ -43,6 +51,12 @@ public final class ConnectPlugin extends JavaPlugin {
         config = new Configuration(this);
         dataStorage = new DataStorage(this);
         getLogger().info("Api ready!");
+
+        getLogger().info("Checking for updates...");
+        checkForUpdates();
+
+        getLogger().info("Setting up Metrics...");
+        this.metrics = new Metrics(this, 26121);
 
         getLogger().info("Checking api key...");
         if (config.getApiKey().equals("YOUR_API_TOKEN")) {
@@ -60,5 +74,37 @@ public final class ConnectPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void checkForUpdates() {
+        getServer().getScheduler().runTaskAsynchronously(this, () -> {
+            try {
+                String apiUrl = "https://api.github.com/repos/Silal123/ConnectPlugin/releases/latest";
+                HttpURLConnection connection = (HttpURLConnection) new URL(apiUrl).openConnection();
+                connection.setRequestProperty("Accept", "application/vnd.github.v3+json");
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder json = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    json.append(line);
+                }
+
+                JsonManager release = new JsonManager(json.toString());
+                String latestVersion = release.getString("tag_name").replace("v", "");
+
+                String currentVersion = getDescription().getVersion();
+
+                if (!currentVersion.equalsIgnoreCase(latestVersion)) {
+                    getLogger().warning("There is a new version available: " + latestVersion + "; Download: https://plugin.conbot.xyz");
+                    getLogger().warning("You are using: " + currentVersion);
+                } else {
+                    getLogger().info("Your plugin is up to date!");
+                }
+
+            } catch (Exception e) {
+                getLogger().warning("Error while checking for updates: " + e.getMessage());
+            }
+        });
     }
 }

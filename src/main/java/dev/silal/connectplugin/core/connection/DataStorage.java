c0a4f6fc6c05b3399c21123c54434881c710da3c
Silal <111568366+Silal123@users.noreply.github.com>
2025-06-08 19:30:36 +0200

    Error message change; added get uuid of a discord id

--- src/main/java/dev/silal/connectplugin/core/connection/DataStorage.java
+++ src/main/java/dev/silal/connectplugin/core/connection/DataStorage.java
@@ -1,9 +1,13 @@
 package dev.silal.connectplugin.core.connection;
 
 import dev.silal.connectplugin.ConnectPlugin;
+import dev.silal.connectplugin.core.errors.UnexpectedConnectionError;
+import dev.silal.connectplugin.core.errors.UnknownUserError;
+import dev.silal.connectplugin.core.utils.Request;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
+import java.io.IOException;
 import java.util.UUID;
 
 public class DataStorage {
@@ -24,5 +28,24 @@ public class DataStorage {
 
     public UserData getPlayer(OfflinePlayer p) {
         return new UserData(plugin, p.getUniqueId());
+    }
+
+    public UUID getUUIDByDiscordId(long id) throws UnexpectedConnectionError, UnknownUserError {
+        try {
+            Request request = new Request(ConnectPlugin.API_BASE + "/account/u/" + id).method(Request.Method.GET).send();
+            int status = request.getResponseCode();
+
+            if (status == 404) {
+                throw new UnknownUserError();
+            }
+
+            if (status != 200 || request.getResponseJson() == null) {
+                throw new UnexpectedConnectionError();
+            }
+
+            return UUID.fromString(request.getResponseJson().getString("uuid"));
+        } catch (IOException | IllegalArgumentException ioex) {
+            throw new UnexpectedConnectionError();
+        }
     }
 }
--- src/main/java/dev/silal/connectplugin/core/errors/UnknownUserError.java
+++ src/main/java/dev/silal/connectplugin/core/errors/UnknownUserError.java
@@ -3,7 +3,7 @@ package dev.silal.connectplugin.core.errors;
 public class UnknownUserError extends Exception {
 
     public UnknownUserError() {
-        super("A user with the given uuid was not found. That means the user is not linked or the uuid is wrong!");
+        super("A user with the given information was not found. That means the user is not linked!");
     }
 
 }
